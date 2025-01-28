256266a13456a92fc9809f47dd4c70fd17ed33b3
laststylebender <ran.mahadik14@gmail.com>
2025-01-28 19:03:19 +0530

    - fix suggestion overlap issue

--- src/prompts/input.rs
+++ src/prompts/input.rs
@@ -1,6 +1,7 @@
 use std::{
     cmp::Ordering,
-    io, iter,
+    io::{self},
+    iter,
     str::FromStr,
     sync::{Arc, Mutex},
 };
@@ -326,6 +327,15 @@ impl<T> Input<'_, T> {
             let line_size = term.size().1 as usize;
             let col = (position + prompt_len) % line_size;
             term.move_cursor_right(col)?;
+
+            if let Some(suggestion) = self.current_suggestions.get(0) {
+                if suggestion.len() - col > 0 {
+                    // hack: when suggestion wraps 2 lines, we can't clear line 2 as line 2 has content that we don't want to clear.
+                    // so idea is to insert a dot at the end of the line and clear it.
+                    term.write_str(".")?;
+                    term.clear_chars(1)?;
+                }
+            }
         }
         Ok(())
     }
